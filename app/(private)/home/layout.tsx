5dcfd11d8099ff15e49fa252805a2077516bc6f7
SOO Ulrich <sessoklinaulrich@gmail.com>
2026-02-11 00:08:08 +0000

    Add loading and empty state components to enhance user experience in PrivateLayout

--- app/(private)/home/layout.tsx
+++ app/(private)/home/layout.tsx
@@ -1,21 +1,41 @@
 "use client";
 
+import {
+  Empty,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from "@/components/ui/empty";
+import { Spinner } from "@/components/ui/spinner";
+
 import { useSession } from "next-auth/react";
-import { redirect, useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 const PrivateLayout = ({ children }: { children: React.ReactNode }) => {
-  const router = useRouter();
   const { data: session, status } = useSession();
 
   if (status === "loading") {
-    return <div>Loading...</div>;
+    return (
+      <Empty className="w-full">
+        <EmptyHeader>
+          <EmptyMedia variant="icon">
+            <Spinner />
+          </EmptyMedia>
+          <EmptyTitle>Processing your request</EmptyTitle>
+          <EmptyDescription>
+            Please wait while we process your request. Do not refresh the page.
+          </EmptyDescription>
+        </EmptyHeader>
+      </Empty>
+    );
   }
 
   if (!session) {
     redirect("/login");
   }
 
-  return { children };
+  return <>{children}</>;
 };
 
 export default PrivateLayout;
